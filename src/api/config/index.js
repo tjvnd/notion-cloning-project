ebf5ef9d4d7a79e81733d25c6873b8c05ac11c7a
tjvnd <ilnotasna@gmail.com>
2022-09-14 01:06:31 +0900

    3rd commit(base)

---
+++ src/api/config/index.js
@@ -0,0 +1,25 @@
+const API_END_POINT = 'https://kdt.roto.codes/documents';
+
+const request = async (url, options = {}) => {
+	try {
+		const res = await fetch(`${API_END_POINT}${url}`, {
+			...options,
+			headers: {
+				'Content-Type': 'application/json',
+				'x-username': 'jiam',
+			},
+		});
+
+		if (res.ok) {
+			return await res.json();
+		}
+
+		throw new Error('API ERROR');
+	} catch (e) {
+		console.log(e.message);
+		history.replaceState(null, null, '/404');
+		window.location = `${window.location.origin}/404`;
+	}
+};
+
+export { request };
